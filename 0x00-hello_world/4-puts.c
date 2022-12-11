e94c007922bee1d5511a050d722fae611a5f03a7
mueez096 <mueez096@gmail.com>
2022-12-11 09:24:50 -0800

    coe

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h> 
-
 /**
- * main- print function
- * Return: 0 zero if successful
+ * main - print function
+ * Return: 0 zero if successful, non-zero
  */
 
-int main(void)
+int  main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"); 
 	return (0);
