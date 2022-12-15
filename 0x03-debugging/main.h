a795ff05fdfb6cdf1a60992e16b541fbc7c5f2ec
mueezzz <119080626+mueezzz@users.noreply.github.com>
2022-12-15 22:13:23 +0100

    Create main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,41 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _positive_or_negative - checks for uppercase character
+ * @i: digit to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _positive_or_negative(int i);
+
+/**
+ * _largest_number - checks for the largest number
+ * @a,b,c: digit to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _largest_number(int a, int b, int c);
+
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
+
+int _convert_day(int month, int day)
+
+
+int _main(void)
+
+
+#endif
