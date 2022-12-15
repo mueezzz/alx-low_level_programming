b019660b7e09c36add4dad166e5400b81c051ad0
mueez096 <mueez096@gmail.com>
2022-12-15 13:49:48 -0800

    win

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,12 +5,11 @@
 * Return: 0
 */
 
-int main(void) 
+int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+	i = 98;
+	positive_or_negative(i);
+	return (0);
 }
