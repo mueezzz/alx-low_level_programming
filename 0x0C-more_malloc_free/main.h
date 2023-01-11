b3f320ce610da312aafa33464445e1fda56df700
mueezzz <119080626+mueezzz@users.noreply.github.com>
2023-01-11 15:50:22 +0100

    Create main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void add_arrays(int *mul_result, int *sum_result, int len_r);
+int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r);
+int is_digit(char c);
+void print_me(int *sum_result, int len_r);
+
+int main(int argc, char **argv);
+
+#endif
